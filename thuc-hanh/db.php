823a5b0cf5d25e070491a543d61afad7f17f8f3b
spxedu <62041731+spxedu@users.noreply.github.com>
2023-03-29 15:45:48 +0700

    Add files via upload

---
+++ thuc-hanh/db.php
@@ -0,0 +1,19 @@
+<?php
+// PHP hỗ trợ các loại thư viện làm việc với SQL: mysql, mysqli, pdo 
+// thực hành với PDO 
+$objConn = null;
+$db_host = '127.0.0.1'; // máy windows xampp mặc định là: localhost
+$db_name = 'thuc_hanh';  // tên CSDL đã tạo ở phpMyAdmin
+$db_user = 'admin'; // máy windows xampp mặc định là root
+$db_pass = '111111'; // // máy windows xampp mặc định là rỗng không viết gì.
+
+try {
+   
+    $objConn = new PDO("mysql:host=$db_host;dbname=$db_name", $db_user, $db_pass);
+    $objConn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+    echo 'Ket noi CSDL thanh cong';
+} catch (Exception $e) {
+    
+    die('Loi ket noi CSDL: '. $e.getMessage() );
+}
