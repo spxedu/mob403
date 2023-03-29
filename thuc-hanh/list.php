ad90892fcb15f6a3797e705c7890af14477cfe51
spxedu <62041731+spxedu@users.noreply.github.com>
2023-03-29 16:15:43 +0700

    Add files via upload

--- thuc-hanh/list.php
+++ thuc-hanh/list.php
@@ -24,4 +24,41 @@ try {
      die( 'Lỗi thực hiện truy vấn CSLD ' . $e->getMessage()  );
 }
 
+// phần dưới dành cho HTML
+?>
 
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+    <h1>Danh sách thể loại</h1>
+    <table border="1">
+
+    <tr>
+        <th>ID</th>   
+        <th>Tên loại</th>
+    </tr>
+
+    <?php  
+        // trong này là phạm vi của PHP
+        foreach( $danh_sach as $k => $row){
+
+                echo "
+                        <tr>
+                            <td>  {$row[ 'id'] }   </td>
+                            <td>   {$row[ 'ten_loai'] }   </td>
+                        </tr>            
+                        ";
+
+        }
+    ?>
+
+    </table>
+</body>
+</html>
