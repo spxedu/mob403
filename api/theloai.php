a4e70e51bd023913e686056c1865c99551c1af85
spxedu <62041731+spxedu@users.noreply.github.com>
2023-03-31 16:03:37 +0700

    Add files via upload

--- api/theloai.php
+++ api/theloai.php
@@ -29,6 +29,48 @@ function listAll(){
 
 }
 
+function addTL(){
+    global $objConn;
+
+    $ten_loai = $_POST['ten_loai'];
+        if(empty ($ten_loai) ){
+            $dataRes =[
+                'status'=>0,
+                'msg'=> 'Chưa nhập tên loại'
+            ]
+  
+        }else{
+            // đã nhập tên loại rồi ==> lưu vào CSDL
+            try {
+
+                $stmt =  $objConn->prepare(
+                    "INSERT INTO the_loai (ten_loai) VALUES (:tham_so_ten);");
+                
+                // gán tham số cho câu lệnh
+                $stmt->bindParam(":tham_so_ten", $ten_loai );
+                // thực thi
+                $stmt->execute();
+ 
+                $dataRes =[
+                    'status'=>1,
+                    'msg'=>  'Đã thêm thành công'
+                ]
+            
+            } catch (PDOException $e) {
+                 
+                $dataRes =[
+                    'status'=>0,
+                    'msg'=> 'Lỗi '. $e->getMessage()
+                ]
+            }
+        }
+
+        die(json_encode ($dataRes ));
+}
+
+
+
+
 //---- xử lý gọi hàm 
 
 $method = $_SERVER['REQUEST_METHOD'];
@@ -38,4 +80,8 @@ if( $method == 'GET'){
     // else{
             // gọi hàm xem chi tiết
     // }
+}
+
+if($method =='POST'){ // đã là post thì chỉ thêm mới, muốn sửa thì dùng PUT
+    addTL();
 }
